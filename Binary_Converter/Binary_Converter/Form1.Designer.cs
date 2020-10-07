b6d968c3bf763f10de6ea7c819c341b791808d22
billblap <65726627+billboblap@users.noreply.github.com>
2020-10-07 03:55:01 -0700

    Add files via upload

---
+++ Binary_Converter/Binary_Converter/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Binary_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TxtDenary = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.TxtBinary = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Btn2Binary = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Btn2Denary = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Hex = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Octaltxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Clear = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.Usless = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtDenary
+            // 
+            this.TxtDenary.Location = new System.Drawing.Point(12, 12);
+            this.TxtDenary.Name = "TxtDenary";
+            this.TxtDenary.Size = new System.Drawing.Size(182, 20);
+            this.TxtDenary.TabIndex = 0;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(9, 35);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(41, 13);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Denary";
+            this.Label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // TxtBinary
+            // 
+            this.TxtBinary.Location = new System.Drawing.Point(12, 56);
+            this.TxtBinary.Name = "TxtBinary";
+            this.TxtBinary.Size = new System.Drawing.Size(182, 20);
+            this.TxtBinary.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Binary";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Btn2Binary
+            // 
+            this.Btn2Binary.Location = new System.Drawing.Point(203, 218);
+            this.Btn2Binary.Name = "Btn2Binary";
+            this.Btn2Binary.Size = new System.Drawing.Size(182, 26);
+            this.Btn2Binary.TabIndex = 4;
+            this.Btn2Binary.Text = "Convert_Binary";
+            this.Btn2Binary.UseVisualStyleBackColor = true;
+            this.Btn2Binary.Click += new System.EventHandler(this.Btn2Binary_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bill Barg 2020";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Btn2Denary
+            // 
+            this.Btn2Denary.Location = new System.Drawing.Point(15, 218);
+            this.Btn2Denary.Name = "Btn2Denary";
+            this.Btn2Denary.Size = new System.Drawing.Size(182, 26);
+            this.Btn2Denary.TabIndex = 6;
+            this.Btn2Denary.Text = "Convert_Denary";
+            this.Btn2Denary.UseVisualStyleBackColor = true;
+            this.Btn2Denary.Click += new System.EventHandler(this.Btn2Denary_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(90, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ツ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txt_Hex
+            // 
+            this.txt_Hex.Location = new System.Drawing.Point(200, 12);
+            this.txt_Hex.Name = "txt_Hex";
+            this.txt_Hex.Size = new System.Drawing.Size(182, 20);
+            this.txt_Hex.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Hexadecimal";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 185);
+            this.progressBar1.Maximum = 80;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(369, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // Octaltxt
+            // 
+            this.Octaltxt.Location = new System.Drawing.Point(201, 55);
+            this.Octaltxt.Name = "Octaltxt";
+            this.Octaltxt.Size = new System.Drawing.Size(181, 20);
+            this.Octaltxt.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(200, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Octal";
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(15, 278);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(366, 23);
+            this.Clear.TabIndex = 14;
+            this.Clear.Text = "Clear All";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(387, 229);
+            this.trackBar1.Maximum = 100000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(197, 45);
+            this.trackBar1.TabIndex = 15;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Usless
+            // 
+            this.Usless.Location = new System.Drawing.Point(387, 280);
+            this.Usless.Name = "Usless";
+            this.Usless.Size = new System.Drawing.Size(197, 20);
+            this.Usless.TabIndex = 16;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(391, 206);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "ClickMe!";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Convert_Hex";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(394, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Btn2Binary;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(630, 315);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Usless);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Octaltxt);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_Hex);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Btn2Denary);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Btn2Binary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtBinary);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.TxtDenary);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtDenary;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.TextBox TxtBinary;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Btn2Binary;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Btn2Denary;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Hex;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox Octaltxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox Usless;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
+
---
+++ Binary_Converter/Binary_Converter/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Binary_Converter
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string HexLetter(int denaryValue)
+        {
+            string Letter = "";
+            //switch statement
+            //faster than an if satement (yandere-dev didnt do this..)
+            switch (denaryValue)
+            {
+                case 10:
+                    Letter = "A";
+                    break;
+                case 11:
+                    Letter = "B";
+                    break;
+                case 12:
+                    Letter = "C";
+                    break;
+                case 13:
+                    Letter = "D";
+                    break;
+                case 14:
+                    Letter = "E";
+                    break;
+                case 15:
+                    Letter = "F";
+                    break;
+                default:
+                    Letter = "Z";
+                    break;
+
+            }
+            //returns hex exivelant based on deanry value 
+            return Letter;
+        }
+
+
+        private string ToHex(int denaryValue)
+        {
+
+            string HexValue = "";
+
+            for (int i = 0; i < 2; i++) //8 bits so must repeat 8 times eg 8++  CAN ONLY USE I J AND K EXCEPT FOR MOUS POS - X, Y
+            {
+                if ((denaryValue % 16) > 9)
+                {
+                    HexValue = HexLetter(denaryValue % 16) + HexValue;
+                    
+                }
+
+                else
+                {
+                    HexValue = Convert.ToString(denaryValue % 16) + HexValue;                  
+                }
+
+                //denary is modded by 2 converted to string then added to binary value
+                denaryValue /= 16;
+
+
+
+            }
+
+            return HexValue;
+
+
+        }
+
+
+        private string to_oct(int denaryValue)
+        {
+            int progressvalue = 0;
+            string octvalue = "";
+
+            for (int i = 0; i < 3; i++) //8 bits so must repeat 8 times eg 8++  CAN ONLY USE I J AND K EXCEPT FOR MOUS POS - X, Y
+            {
+                octvalue = Convert.ToString(denaryValue % 8) + octvalue;
+                //denary is modded by 2 converted to string then added to binary value
+                denaryValue /= 8;
+                progressvalue = progressvalue + 10;
+                progressBar1.Value = progressvalue;
+            }
+
+            return octvalue;
+        }
+
+
+        private string to_binary(int denaryValue)
+        {
+            int progressvalue = 0;
+            string binaryValue = "";
+
+            for (int i = 0; i < 8; i++) //8 bits so must repeat 8 times eg 8++  CAN ONLY USE I J AND K EXCEPT FOR MOUS POS - X, Y
+            {   
+                binaryValue = Convert.ToString(denaryValue % 2) + binaryValue;
+                //denary is modded by 2 converted to string then added to binary value
+                denaryValue /= 2;
+                progressvalue = progressvalue + 10;
+                progressBar1.Value = progressvalue;
+            }
+
+            return binaryValue;
+        }
+       
+
+        private void Btn2Binary_Click(object sender, EventArgs e)
+        {
+            int denaryValue = 0; //holds value for the denary value / just declaring the variable
+            string binaryValue = ""; //holds value for the binary value / just declaring the variable   
+            int maxvalue = 255;
+            //denaryValue = Convert.ToInt16(TxtDenary.Text);
+            //read in text box value convert it to an integer and store
+            try
+            {
+                denaryValue = int.Parse(TxtDenary.Text);//attempt the following code
+                //if dv >= 0 (and) dv <= 255: do(something) 
+                //VALIDATION    
+                if (denaryValue <= 0 | denaryValue > maxvalue)
+                {
+                    TxtDenary.Clear();
+                    TxtBinary.Clear();
+                    Octaltxt.Clear();
+                    MessageBox.Show("ERROR: ('GREATER-THAN-EXPECTED-PLEASE-TRY-<255')");
+                }
+            }
+            catch //run if try failed
+            {
+                TxtDenary.Clear();
+                TxtBinary.Clear();
+                Octaltxt.Clear();
+                MessageBox.Show("ERROR: ('INTEGERS-ONLY')");               
+
+                return; //exit the function
+            }
+
+            txt_Hex.Text = ToHex(denaryValue);  
+            
+            to_binary(denaryValue);
+
+            TxtBinary.Text = to_binary(denaryValue);
+
+            Octaltxt.Text = to_oct(denaryValue);
+
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn2Denary_Click(object sender, EventArgs e)
+        {
+            //ERROR: ('')
+            int progressvalue = 0;
+            int denaryValue = 0; //holds value for the denary value / just declaring the variable
+            int multiplier = 128;
+            string binaryValue = ""; //holds value for the binary value / just declaring the variable
+            char binary_digit;
+            int character_value = 0;
+            binaryValue = TxtBinary.Text;
+            if (binaryValue.Length > 8)
+           
+            {
+                MessageBox.Show("ERROR: ('OUT-OF-RANGE')");
+                progressvalue = 0;
+            }
+
+            if (binaryValue.Length <= 0)
+            {
+                MessageBox.Show("ERROR: ('OUT-OF-RANGE')");
+                progressvalue = 0;
+                return;
+            }
+            for (int i = 0; i < 8; i++) //starts the for loop (loops it 8 times (8bits))
+            {
+                binary_digit = binaryValue[i]; //creates an arry and adds the loop to the array
+                try
+                {
+                    character_value = int.Parse(binary_digit.ToString());//charactervalue is set to a string (so we dont get 48)
+                }
+                catch
+                {
+                    TxtDenary.Clear();
+                    TxtBinary.Clear();
+                    Octaltxt.Clear();
+                    MessageBox.Show("ERROR: ('ENTER-A-CORRECT-BINARY-DIGIT')");
+                }
+                denaryValue += character_value * multiplier;//multiples i by the multipler and then adds to the current value of denary value
+                multiplier /= 2;//divdes by 2 to get to the next value
+                progressvalue = progressvalue + 10;
+                progressBar1.Value = progressvalue;
+
+
+            }
+            TxtDenary.Text = denaryValue.ToString();//puts the value in the text box 
+            Octaltxt.Text = to_oct(denaryValue);
+            txt_Hex.Text = ToHex(denaryValue);
+            //progressBar1.Value = 0;
+            //encapsulation
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            TxtDenary.Clear();
+            TxtBinary.Clear();
+            Octaltxt.Clear();
+            txt_Hex.Clear();
+            progressBar1.Value = 0;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Usless.Text = trackBar1.Value.ToString();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                MessageBox.Show("MESSAGE('CANT-GET-RID')");
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//shut up cracker
